8fe09ab98066eb71837e14ba74509229d44de638
unknown <abdallahaliahmed517@gmail.com>
2024-09-10 01:19:25 +0300

    ddd

--- src/components/Directives/index.vue
+++ src/components/Directives/index.vue
@@ -1,8 +1,10 @@
 <template>
     <div v-awesome.red.big="textToRender"></div>
-    <div v-awesome.blue.small="textToRender"></div>
+    <!-- <div v-awesome.blue.small="textToRender"></div> -->
+    <button @click="textToRender = 'Someting else'">
+        Trigger update
+    </button>
 </template>
-
 <script setup>
     import { ref } from 'vue';
     const textToRender = ref('Some text')
